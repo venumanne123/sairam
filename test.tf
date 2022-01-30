030514c0137b139abd519575ea2c82bec5d10e2b
root <root@ip-172-31-17-127.ec2.internal>
2022-01-30 06:18:42 +0000

    first commit

---
+++ test.tf
@@ -0,0 +1,9 @@
+provide "aws"{
+region = "us-east-2"
+access_key = "AKIAYBZ4DOWLK6DF4C7J"
+secret_key = "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"
+}
+resource "aws_s3_bucket" "def"{
+bucket = "dell12345"
+acl = "private"
+}
